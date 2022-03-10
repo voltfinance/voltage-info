4a82b1f05b132591543d9c7b1d842ec037ed5f3a
mul53 <mulengabowa53@gmail.com>
2022-03-10 15:29:58 +0200

    add voltage subgraph and facotry

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -3,7 +3,7 @@ import { FACTORY_ADDRESS, BUNDLE_ID } from '../constants'
 
 export const SUBGRAPH_HEALTH = gql`
   query health {
-    indexingStatusForCurrentVersion(subgraphName: "fuseio/fuseswap") {
+    indexingStatusForCurrentVersion(subgraphName: "voltfinance/voltage-exchange") {
       synced
       health
       chains {
