fc81c93f966b31aaae239b14e06f65b5e62583c7
Ariel Kotch <ariel@fuse.io>
2024-01-10 14:44:17 +0200

    added v2 support

--- src/hooks/useTVL/usePairs.ts
+++ src/hooks/useTVL/usePairs.ts
@@ -63,6 +63,14 @@ const queryWithPair = gql`
 export const usePair = (numberOfDays, pairAddress) => {
   const [data, setData] = useState([])
   const now = moment().utc()
+  const isV2 = (id, symbol) => {
+    const USDT_V2 = '0x68c9736781e9316ebf5c3d49fe0c1f45d2d104cd'
+    const USDC_V2 = '0x28c3d1cd466ba22f6cae51b1a4692a831696391a'
+    if (USDT_V2?.toLowerCase() === id.toLowerCase() || USDC_V2?.toLowerCase() === id.toLowerCase()) {
+      return symbol + ' V2'
+    }
+    return symbol
+  }
   const fetchPairs = useCallback(async () => {
     setData([])
     try {
@@ -77,7 +85,7 @@ export const usePair = (numberOfDays, pairAddress) => {
       setData(
         data?.pairDayDatas?.map(({ token0, reserveUSD, pairAddress, date, dailyVolumeUSD, token1 }) => {
           return {
-            name: token0?.symbol + '-' + token1?.symbol,
+            name: isV2(token0?.id, token0?.symbol) + '-' + isV2(token1?.id, token1?.symbol),
             id: pairAddress,
             symbol: token0?.symbol + '-' + token1?.symbol,
             totalLiquidityUSD: parseFloat(reserveUSD),
